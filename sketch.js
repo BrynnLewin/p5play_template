1ccc6b5252aed81d41df9887de81f2db60444a13
Brynn Lewin <bhl21@fsu.edu>
2024-12-12 15:29:46 -0500

    cant get shark image to work

--- sketch.js
+++ sketch.js
@@ -1,70 +1,42 @@
-
-
 let shark;
-let highScore = 0;
+let sharksprite;
 let fish;
-let numfish;
-let fishVisible = true;
 
-function preload() {
-    shark = loadImage('assets/shark.png');
-    fish = loadImage('assets/fish.png');
+function preload(){
+  shark = loadImage('assets/shark.png');
 }
 
 function setup() {
-    let canvas = new Canvas(400,400);
+	new Canvas(500, 500);
 
+	fish = new Group();
+	fish.diameter = 10;
+	fish.x = () => random(0, canvas.w);
+	fish.y = () => random(0, canvas.h);
+	fish.amount = 80;
 
+	sharksprite = new Sprite();
+  sharksprite.image = shark;
+  sharksprite.diameter = 30;
 
-	shark = new Sprite();
-    shark.width = 20
-    shark.height = 20 
-    
+	sharksprite.overlaps(fish, collect);
+}
 
-    cursor(createImage(20, 50).loadPixels());
-
-    fish = new Sprite();
-    fish.width = 10;
-    fish.height = 10;
-
-    numfish = int(random(5,21));
-
-
-    textFont("Courier", 24);
+function collect(sharksprite, fish) {
+	fish.remove();
 }
 
 function draw() {
-    background("blue");
 
-    let sharkX = mouseX - shark.width / 2;
-    let sharkY = mouseY - shark.height / 2; 
+  sharksprite.x = mouseX;
+  sharksprite.y = mouseY;
+
+  sharksprite.visible = true;
+
+	clear();
+	sharksprite.moveTowards(mouse);
+
+  background("blue");
 
 
-    image(shark, sharkX, sharkY, shark.width, shark.height); 
-    
-
-    for (let i = 0; i < numFish; i++) {
-
-        let x = random(width - fish.width); 
-        let y = random(height - fish.height);
-        
-     
-        image(fish, x, y);
-      }
-
-    }
-
-    function mousePressed() {
-       
-        if (
-          mouseX > fishX && 
-          mouseX < fishX + fish.width && 
-          mouseY > fishY && 
-          mouseY < fishY + fish.height
-        ) {
-          fishVisible = false; 
-        }
-      }
-
-  //  text("HIGH:  " + (highScore-1), 40, 86);
-
+}
